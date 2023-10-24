7e8a2833de5c55e2b8dca2648fcc2332c6668b4c
S1orc <akulinin.shark2010@yandex.ru>
2023-10-24 14:58:34 +0300

    Версия с кнопками удаления.(Нерабочими)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,21 +14,68 @@ namespace TestSubject.Controllers
             db = context;
         }
 
+        /*        public async Task<IActionResult> Index()
+                {
+                    List<Category> categories = db.Categories.ToList();
+                    List<Product> products = db.Products.ToList();
+                    List<CategoryModel> categoryModels = categories.Select(c => new CategoryModel(c.Id, c.Name, c.ParentId, c.Parent)).ToList();
+                    List<ProductModel> productModels = products.Select(p => new ProductModel(p.Id, p.Name, p.Description, p.CategoryId, p.Category)).ToList();
+
+                    IndexViewModel viewModel = new() { CategoriesModels= categoryModels, ProductsModels = productModels };
+                    return View(viewModel);
+                }*/
+
         public async Task<IActionResult> Index()
         {
-            List<Category> categories = db.Categories.ToList();
-            List<Product> products = db.Products.ToList();
-            List<CategoryModel> categoryModels = categories.Select(c => new CategoryModel(c.Id, c.Name, c.ParentId, c.Parent)).ToList();
-            List<ProductModel> productModels = products.Select(p => new ProductModel(p.Id, p.Name, p.Description, p.CategoryId, p.Category)).ToList();
-
-            IndexViewModel viewModel = new() { CategoriesModels= categoryModels, ProductsModels = productModels };
-            return View(viewModel);
+            var categories = db.Categories.ToList();
+            return View(categories);
         }
 
-        /*        public IActionResult Index()
-                {
-                    var rootCategories = db.Categories.Where(c => c.ParentId == null).ToList();
-                    return View(rootCategories);
-                }*/
+        [HttpGet]
+        public async Task<IActionResult> GetCategory(int? ParentId)
+        {
+            var ResultingCategories = await db.Categories.Where(c => c.ParentId == ParentId).ToListAsync();
+            return PartialView(ResultingCategories);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts(int? categoryId)
+        {
+            var ResultingProducts = await db.Products.Where(p => p.CategoryId == categoryId).ToListAsync();
+            return PartialView("ProductPartial", ResultingProducts);
+        }
+
+/*        [HttpPost]
+        public JsonResult AddProduct(ProductModel newProduct)
+        {
+            return View();
+        }*/
+
+        [HttpPost]
+        public async Task<JsonResult> DeleteProduct(int? productId)
+        {
+            if (productId == null)
+            {
+                return Json(new { success = false, message = "Произошла ошибка при удалении товара!" });
+            }
+
+            var product = await db.Products.Where(p => p.Id == productId).FirstOrDefaultAsync();
+
+            if (product == null)
+            {
+                return Json(new { success = false, message = "Продукт не найден!" });
+            }
+
+            try
+            {
+                db.Products.Remove(product);
+                await db.SaveChangesAsync();
+                return Json(new { success = true, message = "Продукт успешно удален!" });
+            }
+            catch(Exception ex)
+            {
+                return Json(new {success = false, message = ex.Message});
+            }
+        }
     }
 }
